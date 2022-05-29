a9cfcb969b0836cc28207368af702c851d664d0d
TLoske <wurz1@gmx.net>
2022-05-29 17:54:51 +0200

    changed variable name

--- Day 2/tip_calculator.py
+++ Day 2/tip_calculator.py
@@ -2,9 +2,9 @@ print("Welcome to the tip calculator.")
 
 total = float(input("What was the total bill? ")[1:])
 num_people = int(input("How many people to split the bill? "))
-tip_percentage = int(
+tip = int(
     input("What percentage tip would you like to give? 10, 12, or 15? "))
 
-payment = total/num_people*(1 + tip_percentage/100)
+payment = total/num_people*(1 + tip/100)
 
 print(f"Each person should pay: ${payment:.2f}")
