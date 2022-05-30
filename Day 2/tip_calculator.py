2646f903fc0772d2c3fdf0cc48ee6b1e02c40d2c
TLoske <wurz1@gmx.net>
2022-05-30 21:23:30 +0200

    Added Day 3

--- Day 2/tip_calculator.py
+++ Day 2/tip_calculator.py
@@ -1,9 +1,8 @@
 print("Welcome to the tip calculator.")
 
-total = float(input("What was the total bill? ")[1:])
+total = float(input("What was the total bill? $"))
 num_people = int(input("How many people to split the bill? "))
-tip = int(
-    input("What percentage tip would you like to give? 10, 12, or 15? "))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 
 payment = total/num_people*(1 + tip/100)
 
